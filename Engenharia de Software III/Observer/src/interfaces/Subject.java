2b2a6dd5baaba058cef917fd4d0fbafc98b92b45
LuisSCardoso <112117985+LuisSCardoso@users.noreply.github.com>
2024-06-04 11:00:27 -0300

    Delete Engenharia de Software III/Observer/src directory

--- Engenharia de Software III/Observer/src/interfaces/Subject.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface Subject {
-    public void registerObserver(Observer observer); 
-    public void removeObserver(Observer observer);
-    public void notifyObservers(String message);
-}
